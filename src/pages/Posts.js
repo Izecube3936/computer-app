441657394ff05777f0c6695d1598bb633aad764a
Fredrik Johan Dahlbom <fredrik.dahlbom@cmeducations.se>
2022-04-11 23:02:20 +0200

    change

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,27 +1,32 @@
 import React, {useState, useEffect} from 'react'
 import PostItem from '../components/PostItem';
 
+window.onload = function() {
+    fetchAllPuns();
+}
+
 function Posts() {
     const [posts, setPosts] = useState([]); //Korrekt datatyp eftersom listan är en [], tomt värde
     
-    const fetchPosts = async () => {
+    //const fetchPosts = async () => {
+        async function fetchAllPuns() {
         try {
             const response = await fetch('https://puns-app.herokuapp.com/puns');
             const data = await response.json();
-         
+            console.log(data)
             setPosts(data);
             
         } catch (error) {
-            // console.log(error)
+            console.log(error)
             
         } 
     }
 
-    useEffect(() => { fetchPosts() }, [])
+    useEffect(() => { fetchAllPuns() }, [])
     
     return (
         <div>
-            <h1>Posts</h1> 
+            <h1>Puns</h1> 
 
             <section>
                 {
